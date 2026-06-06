656a5148629be474c8f84da292f5cbe9c8b14c54
mikhalexandr <mikhalchenkov22@gmail.com>
2026-06-06 23:45:30 +0300

    ~~~

--- server/src/main/java/dev/mikhalexandr/server/commands/HelpCommand.java
+++ server/src/main/java/dev/mikhalexandr/server/commands/HelpCommand.java
@@ -5,10 +5,8 @@ import dev.mikhalexandr.common.dto.response.CommandResponse;
 import dev.mikhalexandr.server.managers.CommandManager;
 
 public class HelpCommand extends Command {
-  public static final Integer TMP = 8000;
   private static final String CLIENT_EXIT_HELP = "exit";
   private static final String CLIENT_EXIT_DESCRIPTION = "завершить клиентское приложение";
-
   private final CommandManager commandManager;
 
   public HelpCommand(CommandManager commandManager) {
@@ -18,11 +16,6 @@ public class HelpCommand extends Command {
 
   @Override
   public CommandResponse execute(CommandRequest request) {
-    try {
-      Thread.sleep(TMP);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
     StringBuilder builder = new StringBuilder();
     commandManager
         .getCommands()
